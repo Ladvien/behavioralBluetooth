4228115aacc82943e4cf0f5fc6deff2cf2702f79
Casey Brittain <cthomasbrittain@caseys-macbook-pro.local>
2018-08-02 17:02:58 -0500

    Initial Commit

---
+++ BluetoothBehave/BluetoothBehave.h
@@ -0,0 +1,19 @@
+//
+//  BluetoothBehave.h
+//  BluetoothBehave
+//
+//  Created by Casey Brittain on 8/2/18.
+//  Copyright © 2018 Honeysuckle Hardware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BluetoothBehave.
+FOUNDATION_EXPORT double BluetoothBehaveVersionNumber;
+
+//! Project version string for BluetoothBehave.
+FOUNDATION_EXPORT const unsigned char BluetoothBehaveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BluetoothBehave/PublicHeader.h>
+
+
